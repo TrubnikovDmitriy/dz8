6b3bfded06c7b8d03336912df582d9231820670a
TrubnikovDmitriy <trubnikovdv@mail.ru>
2016-11-27 12:45:38 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,11 +8,10 @@ int main(int argc, char* argv[]) {
 	}
 
 	average av(argv[1]);
-	vector<string> filenames;
-	for (int i = 2; i < argc; ++i)
-		filenames.push_back(argv[i]);
-
-	for (auto& i : filenames) av(i);
+	vector<string> filenames(argv + 2, argv + argc);
+	for_each(filenames.begin(), filenames.end(), av);
+	
+//	for (auto& i : filenames) av(i);
 	
 	cout << "Avearage: " << av.get_average() << endl;
 	return EXIT_SUCCESS;
